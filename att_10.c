298d98516129d1bea4b295d19bcb758d61d3627b
Silas Vinícius <svestudos40@gmail.com>
2026-03-24 09:58:46 -0300

    feita

--- att_10.c
+++ att_10.c
@@ -7,9 +7,32 @@ apresente a quantidade de cada cédula que o cliente deverá receber, de forma q
 de cédulas seja o menor possível*/
 
 int main (){
-    setlocale <LC_ALL, "Portuguese">
-    
-    float saque, cedula_5, cedula_10, cedula_20, cedula_50, cedula_100, cedula_200
+    setlocale (LC_ALL, "Portuguese");
+ 
+    int saque, cedula_5, cedula_10, cedula_20, cedula_50, cedula_100, cedula_200;
 
+    printf("Digite o valor do saque: ");
+    scanf("%d", &saque);
+
+    cedula_200 = saque / 200;
+    saque = saque % 200;
+    cedula_100 = saque / 100;
+    saque = saque % 100;
+    cedula_50 = saque / 50;
+    saque = saque % 50;
+    cedula_20 = saque / 20;
+    saque = saque % 20;
+    cedula_10 = saque / 10;
+    saque = saque % 10;
+    cedula_5 = saque / 5;
+    saque = saque % 5;
+
+    printf("Cédulas de 200: %d\n", cedula_200);
+    printf("Cédulas de 100: %d\n", cedula_100);
+    printf("Cédulas de 50: %d\n", cedula_50);
+    printf("Cédulas de 20: %d\n", cedula_20);
+    printf("Cédulas de 10: %d\n", cedula_10);
+    printf("Cédulas de 5: %d\n", cedula_5);
+    
     return 0;
 }
---
+++ att_11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+/* Faça um algoritmo que auxiliará um funcionário de um caixa de supermercado. 
+O algoritmo deverá receber como entrada o valor da compra do cliente e o valor pago pelo cliente,
+em seguida o algoritmo deverá apresentar o troco da compra, de forma que o mesmo deve 
+apresentar a quantidade de cédulas e moedas para compor o troco do cliente. */
+
+int mai(){
+
+    
+
+    return 0;
+}

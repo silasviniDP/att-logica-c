d432da70c98aad398561745505ecb6068ff20d48
Silas Vinícius <svestudos40@gmail.com>
2026-03-26 21:34:16 -0300

    43 sem corpo, para realizar amanhã na sala.

---
+++ Comandos_de_Repetição/att_43.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Um determinado material radioativo perde metade de sua massa a cada 50 segundos. Dada a massa inicial, em gramas, fazer um 
+programa que determine o tempo necessário para que essa massa se torne menor do que 0.5 gramas. 
+Escrever a massa inicial, a massa final e o tempo calculado em segundos. */
+
+int main(){
+
+    
+
+    return 0;
+}
